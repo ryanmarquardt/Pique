5bf22275e3ce08988f0d24103393098c60c345f5
Ryan Marquardt <ryan.marquardt@gmail.com>
2010-04-16 14:48:33 -0700

    Whether or not to echo is now a setting

--- rawtty.py
+++ rawtty.py
@@ -4,15 +4,18 @@ import sys
 import termios
 
 class rawtty(object):
-	def __init__(self, fd=sys.stdin):
+	def __init__(self, fd=sys.stdin, echo=False):
 		if hasattr(fd, 'fileno'):
 			fd = fd.fileno()
 		self.fd = fd
+		self.echo = echo
 		
 	def __enter__(self):
 		self.old = termios.tcgetattr(self.fd)
 		new = termios.tcgetattr(self.fd)
-		new[3] &= ~termios.ICANON & ~termios.ECHO
+		new[3] &= ~termios.ICANON 
+		if not self.echo:
+			new[3] &= ~termios.ECHO
 		termios.tcsetattr(self.fd, termios.TCSANOW, new)
 		
 	def __exit__(self, type, value, traceback):
@@ -20,7 +23,7 @@ class rawtty(object):
 		
 def getch(fd=sys.stdin):
 	with rawtty(fd):
-		return fd.read(1)
+		return fd.read(1, echo=False)
 		
 if __name__=='__main__':
 	print 'Press any key to continue'
