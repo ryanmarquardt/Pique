51eb3a202d7d1799a6f9d4f72a840c8deb9883a5
Ryan Marquardt <ryan.marquardt@gmail.com>
2010-04-17 00:50:03 -0700

    Fixed handling of escape key and other escape sequences Added many key sequences

--- rawtty.py
+++ rawtty.py
@@ -1,8 +1,71 @@
 #!/usr/bin/python
 
+import collections
+import Queue
+import select
 import sys
 import termios
+import threading
 
+ESCAPE = '\x1b'
+
+ALIASES = {
+	'escape':27
+}
+#TABLE = [None]*max(ALIASES.values())
+#for k,v in ALIASES.items():
+	#TABLE[v] = k
+NAMES = {
+	'escape':'\x1b',
+	'eof':'\x04',
+	'left':'\x1b[D',
+	'shift+left':'\x1b[1;2D',
+	'right':'\x1b[C',
+	'shift+right':'\x1b[1;2C',
+	'down':'\x1b[B',
+	'shift+down':'\x1b[1;2B',
+	'up':'\x1b[A',
+	'shift+up':'\x1b[1;2A',
+	'home':'\x1bOH',
+	'end':'\x1bOF',
+	'insert':'\x1b[2~',
+	'shift+insert':'\x1b[2;2~',
+	'delete':'\x1b[3~',
+	'shift+delete':'\x1b[3;2~',
+	'pgup':'\x1b[5~',
+	'pgdn':'\x1b[6~',
+	'f1':'\x1bOP',
+	'shift+f1':'\x1bO1;2P',
+	'f2':'\x1bOQ',
+	'shift+f2':'\x1bO1;2Q',
+	'f3':'\x1bOR',
+	'shift+f3':'\x1bO1;2R',
+	'f4':'\x1bOS',
+	'shift+f4':'\x1bO1;2S',
+	'f5':'\x1b[15~',
+	'shift+f5':'\x1b[15;2~',
+	'f6':'\x1b[17~',
+	'shift+f6':'\x1b[17;2~',
+	'f7':'\x1b[18~',
+	'shift+f7':'\x1b[18;2~',
+	'f8':'\x1b[19~',
+	'shift+f8':'\x1b[19;2~',
+	'f9':'\x1b[20~',
+	'shift+f9':'\x1b[20;2~',
+	'f10':'\x1b[21~',
+	'shift+f10':'\x1b[21;2~',
+	'f11':'\x1b[23~',
+	'shift+f11':'\x1b[23;2~',
+	'f12':'\x1b[24~',
+	'shift+f12':'\x1b[24;2~',
+	'bksp':'\x7f',
+	'tab':'\t',
+	'shift+tab':'\x1b[Z',
+}
+for k,v in NAMES.items():
+	NAMES[v] = k
+KNOWN_SEQUENCES = NAMES.values()
+	
 class rawtty(object):
 	def __init__(self, fd=sys.stdin, echo=False):
 		if hasattr(fd, 'fileno'):
@@ -21,10 +84,56 @@ class rawtty(object):
 	def __exit__(self, type, value, traceback):
 		termios.tcsetattr(self.fd, termios.TCSADRAIN, self.old)
 		
-def getch(fd=sys.stdin):
+class EOF(Exception):
+	pass
+	
+def keypresses(fd=sys.stdin, echo=False, timeout=1):
+	q = Queue.Queue()
+	def readthread():
+		c = True
+		with rawtty(fd=fd, echo=echo):
+			while c:
+				c = fd.read(1)
+				q.put(c)
+	ReadThread = threading.Thread(target=readthread)
+	ReadThread.daemon = True
+	ReadThread.start()
+	while True:
+		seq = q.get()
+		if not seq:
+			raise EOF
+		elif seq == ESCAPE:
+			try:
+				seq += q.get(timeout=timeout)
+			except Queue.Empty:
+				yield seq
+				#Assume that only escape was pressed
+			else:
+				#Probably not the escape key by itself
+				#Continue reads until we have a full sequence or error
+				while any(s.startswith(seq) for s in KNOWN_SEQUENCES):
+					if seq not in KNOWN_SEQUENCES:
+						seq += q.get()
+					else:
+						break
+				if seq in KNOWN_SEQUENCES:
+					yield seq
+				else:
+					#No match
+					raise IOError('Unrecognized Sequence %r' % seq)
+		else:
+			yield seq
+
+def getch(fd=sys.stdin, echo=False):
 	with rawtty(fd):
-		return fd.read(1, echo=False)
+		return fd.read(1)
 		
 if __name__=='__main__':
-	print 'Press any key to continue'
-	getch()
+	print KNOWN_SEQUENCES
+	while True:
+		try:
+			for key in keypresses():
+				print repr(NAMES.get(key,key))
+		except IOError, e:
+			print e
+			break
