7e0ae23b46245b5a7cfef6b0be18eb2ef5a4cbdf
Orbnauticus <ryan.marquardt@gmail.com>
2010-06-14 14:07:11 -0700

    Created initial packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='pique', version='0.01',
+	author='Ryan Marquardt',
+	author_email='ryan.marquardt@gmail.com',
+	description='Pique Media Center',
+	url='http://orbnauticus.github.org/pique',
+	packages=['pique'],
+)
