ef90bf70f0f4e3a5bd0a3179257342d283cb5890
Orbnauticus <ryan.marquardt@gmail.com>
2010-06-14 14:57:05 -0700

    More packaging fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+
 setup(name='pique', version='0.01',
 	author='Ryan Marquardt',
 	author_email='ryan.marquardt@gmail.com',
 	description='Pique Media Center',
 	url='http://orbnauticus.github.org/pique',
 	packages=['pique'],
+	package_data={'pique': ['table-def.conf']},
+	license='Simplified BSD License',
+	scripts=['piqued'] + [os.path.join('tools',s) for s in os.listdir('tools')],
 )
