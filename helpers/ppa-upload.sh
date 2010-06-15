6d948c95e74dea66cdffd7ae7aacfbcf143381aa
Orbnauticus <ryan.marquardt@gmail.com>
2010-06-14 18:33:59 -0700

    Launchpad now accepts source package

--- helpers/ppa-upload.sh
+++ helpers/ppa-upload.sh
@@ -3,3 +3,4 @@ cd ..
 tar -xz < Pique/dist/pique-0.01.tar.gz
 cd pique-0.01
 debuild -S -sa
+dput ppa:ryan-marquardt/ppa ../pique_0.01_source.changes
--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@
 from distutils.core import setup
 import os
 
+def find(path='.'):
+	r = []
+	for f in os.listdir(path):
+		if os.path.isdir(f):
+			r.extend(find(f))
+		else:
+			r.append(os.path.join(path, f))
+	return r
+
 setup(name='pique', version='0.01',
 	author='Ryan Marquardt',
 	author_email='ryan.marquardt@gmail.com',
@@ -11,5 +20,5 @@ setup(name='pique', version='0.01',
 	packages=['pique'],
 	package_data={'pique': ['table-def.conf']},
 	license='Simplified BSD License',
-	scripts=['piqued'] + [os.path.join('tools',s) for s in os.listdir('tools')],
+	scripts=['piqued'] + find('tools'),
 )
