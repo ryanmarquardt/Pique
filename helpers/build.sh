fad6cc5e4820561baf57df15e8273c2dd7525410
Orbnauticus <ryan.marquardt@gmail.com>
2010-06-16 11:37:33 -0700

    Builds happen in dist/

--- helpers/build.sh
+++ helpers/build.sh
@@ -1,35 +1,33 @@
 export PY_FULLNAME=$(python setup.py --fullname)
 export PACKAGE_VERSION=$(grep ^$(python setup.py --name) debian/changelog | head -n 1 | cut -d\( -f2 | cut -d\) -f1)
 export PACKAGE_ARCH=$(dpkg-architecture -qDEB_BUILD_ARCH)
+DEBUILD=$(which debuild)
 
-verbose () {
-	echo BUILD.SH "$@" >&2
-	"$@"
-}
+verbose () { echo BUILD.SH "$@" >&2 ; "$@" ; }
+indir () { ( cd "$1" ; shift ; "$@" ) ; }
 
-pack () {
-	python setup.py sdist
-}
+pack () { python setup.py sdist ; }
 
 unpack () {
 	pack
-	verbose tar -xf "dist/${PY_FULLNAME}.tar.gz"
-	echo "${PY_FULLNAME}"
+	indir dist verbose tar -xf "${PY_FULLNAME}.tar.gz"
 }
+
+debuild () { indir "${PY_FULLNAME}" $DEBUILD "$@" ; }
 
 deb-source () {
 	unpack
-	( cd "${PY_FULLNAME}"; debuild -S -sa )
+	indir "dist/${PY_FULLNAME}" debuild -S -sa
 }
 
 deb-source-diff () {
 	unpack
-	( cd "${PY_FULLNAME}"; debuild -S -sd )
+	indir "dist/${PY_FULLNAME}" debuild -S -sd
 }
 
 deb-binary () {
 	unpack
-	( cd "${PY_FULLNAME}"; debuild )
+	indir "dist/${PY_FULLNAME}" debuild
 }
 
 deb () {
