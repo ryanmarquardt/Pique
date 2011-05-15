c497c1346c6bdde8721149fccbe4fa7cca051b32
Ryan Marquardt (orbnauticus) <ryan.marquardt@gmail.com>
2011-05-15 13:25:37 +0900

    Fixed find function in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from itertools import chain
 import os
 
 def find(path='.'):
-	r = []
-	for f in os.listdir(path):
-		if os.path.isdir(f):
-			r.extend(find(f))
-		else:
-			r.append(os.path.join(path, f))
-	return r
+	return list(chain(*[find(x) if os.path.isdir(x) else (x,) for x in map(lambda x:os.path.join(path,x),os.listdir(path))]))
 
 setup(name='pique', version='0.02.3',
 	author='Ryan Marquardt',
@@ -18,7 +13,7 @@ setup(name='pique', version='0.02.3',
 	description='Pique Media Center',
 	url='http://orbnauticus.github.org/pique',
 	packages=['pique'],
-	package_data={'pique': ['table-def.conf', 'default.conf']},
+	package_data={'pique': ['default.conf']},
 	license='Simplified BSD License',
 	scripts=['piqued'] + find('tools'),
 )
